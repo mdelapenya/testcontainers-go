fb2c0aa7a6d168d5e2f43e392b9f7c0fb7681da8
Manuel de la Peña <mdelapenya@gmail.com>
2025-04-14 11:14:44 +0200

    chore: move options to its file

--- modules/firebase/firebase.go
+++ modules/firebase/firebase.go
@@ -25,38 +25,6 @@ const rootFilePath = "/srv/firebase"
 // ErrRootNotProvided is returned when the root path is not provided
 var ErrRootNotProvided = errors.New("firebase root not provided (WithRoot is required)")
 
-// WithRoot sets the directory which is copied to the destination container as firebase root
-func WithRoot(rootPath string) testcontainers.CustomizeRequestOption {
-	return testcontainers.WithFiles(testcontainers.ContainerFile{
-		HostFilePath:      rootPath,
-		ContainerFilePath: rootFilePath,
-		FileMode:          0o775,
-	})
-}
-
-// WithData names the data directory (by default under firebase root), can be used as a way of setting up fixtures.
-// Usage of absolute path will imply that the user knows how to mount external directory into the container.
-func WithData(dataPath string) testcontainers.CustomizeRequestOption {
-	return func(req *testcontainers.GenericContainerRequest) error {
-		req.Env["DATA_DIRECTORY"] = dataPath
-		return nil
-	}
-}
-
-const cacheFilePath = "/root/.cache/firebase"
-
-// WithCache enables firebase binary cache based on session (meaningful only when multiple tests are used)
-func WithCache() testcontainers.CustomizeRequestOption {
-	volumeName := fmt.Sprintf("firestore-cache-%s", testcontainers.SessionID())
-
-	return testcontainers.WithMounts(testcontainers.ContainerMount{
-		Source: testcontainers.DockerVolumeMountSource{
-			Name: volumeName,
-		},
-		Target: cacheFilePath,
-	})
-}
-
 func gatherPorts(config partialFirebaseConfig) ([]string, error) {
 	var ports []string
 
